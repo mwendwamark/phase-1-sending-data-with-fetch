34f64aabcb471072d5e63d1c32b2eeca1364c4d2
mark mwendwa <mwendwamark310@gmail.com>
2023-03-27 19:32:43 +0300

    first commit

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
-// Add your code here
+function submitData(personsName,personsEmail){
+    const data ={
+        name:personsName,
+        email:personsEmail
+    }
+    const configObj = {
+        method:"POST",
+        headers: {
+            "Content-Type":"application/json",
+            Accept:"application/json"
+        },
+        body: JSON.stringify(data),    
+    }
+    let j = document.createElement("j")
+
+    return fetch("http://localhost:3000/users", configObj)
+    .then(function(response){
+        return response.json();
+    })
+    .then(data =>{
+        let j = document.createElement("j")
+        j.textContent = data.name + " " + data.email
+        document.querySelector("body").appendChild(j)
+    })
+    .catch(function(error){
+        let j = document.createElement("j")
+        j.textContent = error.message;
+        document.querySelector("body").appendChild(j)
+    })
+}
